389dccfdb2ec894885be6819a4f695c12d969f5a
Dkris <>
2023-02-23 15:32:10 +0000

    Initial commit

---
+++ main (copy).sh
@@ -0,0 +1,19 @@
+#
+#read user input
+#echo user input
+#!/bin/bash
+
+echo "Hey how are you"
+read feeling
+
+echo "That's good to know, what do you want to eat?"
+read food
+
+echo "yes, I love $food"
+
+echo "Do you know a good place?"
+
+read place
+
+echo "$place has the best $food ever."
+echo "Lets go!! I'm staving."
